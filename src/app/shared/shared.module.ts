6cb6a02ea7fb6e7c7d40451107f9235088291312
Woon Tsou <woon.tsou@janeirodigital.com>
2018-08-23 14:25:42 -0600

    base data-table created

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
+import { PrimeNgModule } from './primeng.module';
 import { NgBootstrapModule } from './ng-bootstrap.module';
 import { IconsModule } from './icons.module';
 import { LayoutComponent } from './components/layout/layout.component';
@@ -11,12 +12,14 @@ import { FooterComponent } from './components/footer/footer.component';
 import { PageComponent } from './components/page/page.component';
 import { MinHeightLayoutCalcDirective } from './directives/minHeightLayoutCalc.directive';
 import { SubnavComponent } from './components/subnav/subnav.component';
+import { DataTableComponent } from './components/data-table/data-table.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
     FormsModule,
+    PrimeNgModule,
     NgBootstrapModule,
     IconsModule,
     RouterModule
@@ -25,10 +28,12 @@ import { SubnavComponent } from './components/subnav/subnav.component';
     CommonModule,
     ReactiveFormsModule,
     FormsModule,
+    PrimeNgModule,
     NgBootstrapModule,
     IconsModule,
     PageComponent,
-    SubnavComponent
+    SubnavComponent,
+    DataTableComponent
   ],
   declarations: [
     LayoutComponent,
@@ -36,7 +41,8 @@ import { SubnavComponent } from './components/subnav/subnav.component';
     FooterComponent,
     PageComponent,
     MinHeightLayoutCalcDirective,
-    SubnavComponent
+    SubnavComponent,
+    DataTableComponent
   ],
   providers: [],
 })
