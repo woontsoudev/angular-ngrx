083895ee63e632010594a32253cf0a7dd6841572
Woon Tsou <woon.tsou@janeirodigital.com>
2018-08-22 23:21:59 -0600

    Layout improvements

--- src/app/views/ui-library/components/ui-interaction/ui-interaction.component.ts
+++ src/app/views/ui-library/components/ui-interaction/ui-interaction.component.ts
@@ -2,23 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { Store } from '@ngrx/store';
 
-import * as ConuterActions from '../../../actions/counter.actions';
-import * as CounterReducer from '../../../reducers/counter.reducer';
+import * as ConuterActions from '../../../../actions/counter.actions';
+import * as CounterReducer from '../../../../reducers/counter.reducer';
 
 @Component({
   selector: 'app-ui-interaction',
-  template: `
-  <h2 class="title">Redux example</h2>
-  <div class="row">
-    <div class="col">
-      <article>
-        <button class="btn btn-danger mr-1 mb-1" (click)="decrement()">Decrement</button>
-        <button class="btn btn-primary" (click)="increment()">Increment</button>
-        <p>count: {{count$ | async}}</p>
-      </article>
-    </div>
-  </div>
-  `
+  templateUrl: './ui-interaction.component.html'
 })
 
 export class UiInteractionComponent implements OnInit {
