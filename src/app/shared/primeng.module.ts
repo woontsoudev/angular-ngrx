ee808b47eb146da07b3ec37523f5eee81e34d306
Woon Tsou <woon.tsou@janeirodigital.com>
2018-08-24 00:04:04 -0600

    general changes and improvements on layout

--- src/app/shared/primeng.module.ts
+++ src/app/shared/primeng.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { TableModule } from 'primeng/table';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @NgModule({
-  exports: [TableModule]
+  exports: [TableModule, CheckboxModule]
 })
 export class PrimeNgModule { }
