98537e9de752ac403d4d8b65e5b363dcf356abf5
Woon Tsou <woon.tsou@janeirodigital.com>
2018-08-28 09:53:15 -0600

    prettier configuration added

--- src/app/shared/icons.module.ts
+++ src/app/shared/icons.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 
-import { faAngry, faBell, faUser, faSyncAlt, faSearch, faBars } from '@fortawesome/free-solid-svg-icons';
+import {
+  faAngry,
+  faBell,
+  faUser,
+  faSyncAlt,
+  faSearch,
+  faBars
+} from '@fortawesome/free-solid-svg-icons';
 
 library.add(faAngry, faBell, faUser, faSyncAlt, faSearch, faBars);
 
 @NgModule({
   exports: [FontAwesomeModule]
 })
-export class IconsModule { }
+export class IconsModule {}
