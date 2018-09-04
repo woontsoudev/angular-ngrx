e8d8a40ae93fe3075be2b0a99d205674bae8ee76
janeirodigital <mario.rodriguez@janeirodigital.com>
2018-09-04 14:11:23 -0600

    jmr/FFB-130: Shared action buttons component created

---
+++ src/app/shared/components/action-buttons/action-buttons.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-action-buttons",
+  templateUrl: "./action-buttons.component.html",
+  styleUrls: ["./action-buttons.component.scss"]
+})
+export class ActionButtonsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,20 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 
-import { PrimeNgModule } from './primeng.module';
-import { NgBootstrapModule } from './ng-bootstrap.module';
-import { IconsModule } from './icons.module';
-import { LayoutComponent } from './components/layout/layout.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { PageComponent } from './components/page/page.component';
-import { MinHeightLayoutCalcDirective } from './directives/minHeightLayoutCalc.directive';
-import { SubnavComponent } from './components/subnav/subnav.component';
-import { DataTableComponent } from './components/data-table/data-table.component';
-import { ActionBarComponent } from './components/action-bar/action-bar.component';
-import { TitleBarComponent } from './components/title-bar/title-bar.component';
+import { PrimeNgModule } from "./primeng.module";
+import { NgBootstrapModule } from "./ng-bootstrap.module";
+import { IconsModule } from "./icons.module";
+import { LayoutComponent } from "./components/layout/layout.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { PageComponent } from "./components/page/page.component";
+import { MinHeightLayoutCalcDirective } from "./directives/minHeightLayoutCalc.directive";
+import { SubnavComponent } from "./components/subnav/subnav.component";
+import { DataTableComponent } from "./components/data-table/data-table.component";
+import { ActionBarComponent } from "./components/action-bar/action-bar.component";
+import { TitleBarComponent } from "./components/title-bar/title-bar.component";
+import { NotificationsComponent } from "./components/notifications/notifications.component";
+import { ActionButtonsComponent } from "./components/action-buttons/action-buttons.component";
 
 @NgModule({
   imports: [
@@ -48,7 +50,9 @@ import { TitleBarComponent } from './components/title-bar/title-bar.component';
     SubnavComponent,
     DataTableComponent,
     ActionBarComponent,
-    TitleBarComponent
+    TitleBarComponent,
+    NotificationsComponent,
+    ActionButtonsComponent
   ],
   providers: []
 })
