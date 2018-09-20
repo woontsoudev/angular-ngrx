2c8d2b4e207b62ec400c5239442151ba5dad244e
Woon Tsou <woon.tsou@janeirodigital.com>
2018-09-20 17:59:05 -0600

    model added

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,4 +1,18 @@
-export interface User {
+export interface Role {
   id: string;
   name: string;
+}
+
+export interface User {
+  id: string;
+  role: Role;
+  username: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  addressLine1: string;
+  addressLine2?: string;
+  phone?: string;
+  created?: string;
 }
