fe3dff609d110a7c3b291a14cd5aadc39ece3960
Woon Tsou <woon.tsou@janeirodigital.com>
2018-09-20 16:27:47 -0600

    subnav and router implementation

--- src/app/views/admin/admin-routing.module.ts
+++ src/app/views/admin/admin-routing.module.ts
@@ -2,11 +2,30 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AdminComponent } from './admin.component';
+import { UsersComponent } from './components/users/users.component';
+import { ConfigurationComponent } from './components/configuration/configuration.component';
+import { ReportComponent } from './components/report/report.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: AdminComponent
+    redirectTo: 'users',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    component: AdminComponent,
+    children: [
+      { path: '', redirectTo: 'users', pathMatch: 'full' },
+      { path: 'users', component: UsersComponent },
+      { path: 'configuration', component: ConfigurationComponent },
+      { path: 'report', component: ReportComponent }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'users',
+    pathMatch: 'full'
   }
 ];
 
--- src/app/views/admin/admin.module.ts
+++ src/app/views/admin/admin.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 import { AdminRoutingModule } from './admin-routing.module';
 import { SharedModule } from '../../shared/shared.module';
 import { AdminComponent } from './admin.component';
+import { UsersComponent } from './components/users/users.component';
+import { ConfigurationComponent } from './components/configuration/configuration.component';
+import { ReportComponent } from './components/report/report.component';
 
 @NgModule({
   imports: [AdminRoutingModule, SharedModule],
   exports: [],
-  declarations: [AdminComponent],
+  declarations: [
+    AdminComponent,
+    UsersComponent,
+    ConfigurationComponent,
+    ReportComponent
+  ],
   providers: []
 })
 export class AdminModule {}
