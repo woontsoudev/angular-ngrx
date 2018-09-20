1132efee4a913d6b3771aed7333891eec54d2622
Woon Tsou <woon.tsou@janeirodigital.com>
2018-09-20 17:48:15 -0600

    datatable added

--- src/app/views/admin/admin.component.ts
+++ src/app/views/admin/admin.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin',
-  templateUrl: 'admin.component.html'
+  templateUrl: 'admin.component.html',
+  styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
   constructor() {}
--- src/app/views/admin/components/users/users.component.ts
+++ src/app/views/admin/components/users/users.component.ts
@@ -1,11 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   selector: 'app-users',
   templateUrl: 'users.component.html'
 })
 export class UsersComponent implements OnInit {
+  public cols = [
+    { field: 'name', header: 'Name' },
+    { field: 'email', header: 'Email' },
+    { field: 'role', header: 'Role' },
+    { field: 'properties', header: 'Properties' },
+    { field: 'lastLogin', header: 'Last Login' }
+  ];
+  public $users: Observable<any[]>;
+
   constructor() {}
 
   ngOnInit() {}
+
+  onRowSelect(event) {
+    console.log(event);
+  }
+
+  onRowDelete(event) {
+    console.log(event);
+  }
+
+  onAddUser() {
+    console.log();
+  }
 }
